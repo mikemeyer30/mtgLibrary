85e3c9d47c40d94bfe86727d29dd5dd904ec5bcb
stonebrain <michaelmeyer@149-162-173-31.dhcp-in.iupui.edu>
2019-11-07 13:18:15 -0500

    Added utils and pages folders

---
+++ src/pages/Main.js
@@ -0,0 +1,18 @@
+// import React, { Component } from 'react';
+// import AppBar from '@material-ui/core/AppBar';
+
+// class Main extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     }
+//   }
+
+//   render() {
+//     const { styles, theme } = this.props;
+//     return(
+//       <AppBar></AppBar>
+//     )
+//   };
+// }
